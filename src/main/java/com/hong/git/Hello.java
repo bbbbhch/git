cfbbccc4ca9be7b9811d30e1aa6d64b9dae02acf
hong <485865188@qq.com>
2019-10-15 11:19:29 +0800

    idea 第一次提交

--- src/main/java/com/hong/git/Hello.java
+++ src/main/java/com/hong/git/Hello.java
@@ -6,5 +6,6 @@ package com.hong.git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("idea下写的代码");
     }
 }
