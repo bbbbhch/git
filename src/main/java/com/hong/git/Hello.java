6e3306a07494a0f56c044211f5c98655ec66bb79
hong <485865188@qq.com>
2019-10-15 11:20:56 +0800

    master修改代码

--- src/main/java/com/hong/git/Hello.java
+++ src/main/java/com/hong/git/Hello.java
@@ -6,5 +6,6 @@ package com.hong.git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("master提交的代码");
     }
 }
